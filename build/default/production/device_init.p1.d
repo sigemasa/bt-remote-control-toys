b4bdd085fde986f76921c5494ebb57a3e835fb67
Yoshinori.Shigemasa <Yoshinori.Shigemasa@gmail.com>
2015-01-24 05:31:15 +0000

    バージョン管理を開始：  対応している車体バージョン 0.9(Backward and Forward)

---
+++ build/default/production/device_init.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/device_init.d  \
+ build/default/production/device_init.p1:  \
+ device_init.c 
---
+++ build/default/production/motor_pwm.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/motor_pwm.d  \
+ build/default/production/motor_pwm.p1:  \
+ motor_pwm.c 
